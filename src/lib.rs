6bf9f936ab567674f2cdce40c76c4333a23e500e
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-04-13 18:44:02 +0700

    test(strategy-di): implement test for strategy_di

--- src/lib.rs
+++ src/lib.rs
@@ -73,3 +73,25 @@ pub mod strategy_di {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::strategy_di;
+
+    #[test]
+    fn test_strategy_di() {
+        let mysql_db = strategy_di::MySQLDatabase;
+        let addition = strategy_di::AdditionStrategy;
+        let data_service = strategy_di::DataService::new(mysql_db);
+        let context = strategy_di::Context::new(addition, data_service);
+
+        assert_eq!(context.execute(2, 3), "MySQL: SELECT 5;");
+
+        let postgres_db = strategy_di::PostgresDatabase;
+        let subtraction = strategy_di::SubtractionStrategy;
+        let data_service = strategy_di::DataService::new(postgres_db);
+        let context = strategy_di::Context::new(subtraction, data_service);
+
+        assert_eq!(context.execute(10, 3), "Postgres: SELECT 7;");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-use design_patterns::strategy_di;
-
-fn main() {
-    let mysql_db = strategy_di::MySQLDatabase;
-    let addition = strategy_di::AdditionStrategy;
-    let data_service = strategy_di::DataService::new(mysql_db);
-    let context = strategy_di::Context::new(addition, data_service);
-
-    println!("{}", context.execute(2, 3));
-
-    let postgres_db = strategy_di::PostgresDatabase;
-    let subtraction = strategy_di::SubtractionStrategy;
-    let data_service = strategy_di::DataService::new(postgres_db);
-    let context = strategy_di::Context::new(subtraction, data_service);
-
-    println!("{}", context.execute(10, 3));
-}
+fn main() {}
